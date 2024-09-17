a3df42d3670bd91dba6cd6e46ccb35cbe37b9102
itzxko <134848015+itzxko@users.noreply.github.com>
2024-09-17 22:24:58 +0800

    Initial commit

---
+++ Frontend/Bottle-Bot/app/(tabs)/redeem.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const redeem = () => {
+  return (
+    <View>
+      <Text>redeem</Text>
+    </View>
+  );
+};
+
+export default redeem;
---
+++ Frontend/Bottle-Bot/app/index.jsx
@@ -0,0 +1,64 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { Link } from "expo-router";
+
+export default function index() {
+  return (
+    <View className="flex-1 w-full bg-black">
+      <View className="w-full h-[30vh] bg-black"></View>
+      <View className="w-full h-[80vh] bg-white rounded-t-[40px] items-center justify-center px-8">
+        <View className="w-full flex items-center justify-center pb-6">
+          <Text className="text-xl font-semibold tracking-wider">
+            Account Login.
+          </Text>
+          <Text className="text-xs font-normal text-gray-500">
+            Log in to your account to continue using the app
+          </Text>
+        </View>
+
+        <View className="w-full flex items-start justify-center py-6">
+          <Text className="font-semibold text-sm pb-2">Username:</Text>
+          <TextInput
+            className="bg-white w-full border border-black px-4 py-2 rounded-md"
+            placeholder="Enter your email"
+          />
+        </View>
+        <View className="w-full flex items-start justify-center pb-6">
+          <Text className="font-semibold text-sm pb-2">Password:</Text>
+          <TextInput
+            className="bg-white w-full border border-black px-4 py-2 rounded-md"
+            placeholder="Enter your password"
+            textContentType={"password"}
+            secureTextEntry={true}
+          />
+        </View>
+
+        <View className="w-full flex items-end justify-center">
+          <Text className="font-semibold text-xs pb-2">Forgot Password?</Text>
+        </View>
+        <View className="w-full flex items-center justify-center py-6">
+          <TouchableOpacity
+            className="bg-black w-full px-4 py-3 rounded-md"
+            activeOpacity={0.8}
+          >
+            <Link
+              href="/dashboard"
+              className="text-white text-center text-sm font-semibold"
+            >
+              Login
+            </Link>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
