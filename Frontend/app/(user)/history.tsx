1ea48ebd64a8d5e9294e4993be74d1babe57ac42
itzxko <134848015+itzxko@users.noreply.github.com>
2024-10-02 18:32:00 +0800

    updated history

--- Frontend/app/(user)/history.tsx
+++ Frontend/app/(user)/history.tsx
@@ -16,6 +16,7 @@ import { useAuth } from "@/context/AuthContext";
 import Loader from "@/components/loader";
 import { LinearGradient } from "expo-linear-gradient";
 import { useHistory } from "@/context/UserHistoryProvider";
+import { Image } from "expo-image";
 
 const History = () => {
   // const [rewardsHistory, setRewardsHistory] = useState([]);
@@ -57,6 +58,8 @@ const History = () => {
 
   interface RedeemableItem {
     rewardName: string;
+    _id: string;
+    image: string;
   }
 
   interface Item {
@@ -165,45 +168,97 @@ const History = () => {
                       </Text>
                     </View>
                   ) : (
-                    rewardsHistory.map((rewardsHistory: RewardsHistory) => {
-                      const redeemableItem = redeemables.find(
-                        (item: Item) => item._id === rewardsHistory.rewardId
-                      ) as RedeemableItem | undefined;
-                      const rewardName = redeemableItem
-                        ? redeemableItem.rewardName
-                        : "Not Found";
+                    rewardsHistory.map(
+                      (rewardsHistory: RewardsHistory, index: any) => {
+                        const reward = redeemables.find(
+                          (reward: RedeemableItem) =>
+                            reward._id === rewardsHistory.rewardId
+                        ) as RedeemableItem | undefined;
 
-                      return (
-                        <View
-                          className="w-full bg-[#E6E6E6] p-6 rounded-xl mb-4"
-                          key={rewardsHistory._id}
-                        >
-                          <View className="w-full flex flex-row justify-between items-center pb-3">
-                            <Text className="text-xs font-normal text-black/50">
-                              {(() => {
-                                const date = new Date(
-                                  rewardsHistory.dateClaimed
-                                );
-                                return isNaN(date.getTime())
-                                  ? "Invalid Date"
-                                  : date.toLocaleDateString("en-US"); // Format the date as needed
-                              })()}
-                            </Text>
-                            <Text className="text-xs font-normal text-black/50 uppercase">
-                              #{rewardsHistory._id}
-                            </Text>
+                        const backgroundColor =
+                          index % 2 === 0
+                            ? ["#00674F", "#06402B"]
+                            : ["#D2AF26", "#BE8400"];
+
+                        return (
+                          <View
+                            className="w-full flex-row items-center justify-center bg-[#E6E6E6] mb-4 rounded-xl overflow-hidden"
+                            key={rewardsHistory._id}
+                          >
+                            <LinearGradient
+                              colors={backgroundColor}
+                              className="w-[3%] h-full"
+                            ></LinearGradient>
+                            <View className="w-[97%] flex items-start justify-center px-6 py-4">
+                              <View className="w-full flex flex-row justify-between items-start">
+                                <View className="w-1/2 flex items-start justify-center">
+                                  {user ? (
+                                    <Text className="text-xs font-normal text-black/50 ">
+                                      {`${user.personalInfo.firstName} ${user.personalInfo.lastName}`}
+                                    </Text>
+                                  ) : (
+                                    <Text className="text-xs font-normal text-black/50">
+                                      User not Found
+                                    </Text>
+                                  )}
+                                  {reward ? (
+                                    <Text className="text-lg font-semibold capitalize text-[#00674F]">
+                                      {reward.rewardName}
+                                    </Text>
+                                  ) : (
+                                    <Text className="text-lg font-semibold capitalize text-[#00674F]">
+                                      Reward Item Not Found
+                                    </Text>
+                                  )}
+                                </View>
+                                <Text
+                                  className="w-1/2 text-right text-xs font-normal text-black/50 uppercase"
+                                  numberOfLines={1}
+                                >
+                                  {rewardsHistory._id}
+                                </Text>
+                              </View>
+                              <View className="w-full flex flex-row justify-between items-end pt-4">
+                                <Text
+                                  className="w-1/3 text-left text-xs font-normal text-black/50"
+                                  numberOfLines={1}
+                                >
+                                  {(() => {
+                                    const date = new Date(
+                                      rewardsHistory.dateClaimed
+                                    );
+                                    return isNaN(date.getTime())
+                                      ? "Invalid Date"
+                                      : date.toLocaleDateString("en-US"); // Format the date as needed
+                                  })()}
+                                </Text>
+
+                                <View className="w-2/3 flex flex-row-reverse items-center justify-start">
+                                  <Text className="text-xs font-semibold text-black pl-1">
+                                    {rewardsHistory.pointsSpent}
+                                    {rewardsHistory.pointsSpent > 1
+                                      ? "pts."
+                                      : "pt."}
+                                  </Text>
+                                  {reward ? (
+                                    <View className="w-[40px] h-[40px] flex items-center justify-center rounded-full bg-black overflow-hidden">
+                                      <Image
+                                        className="w-full flex-1"
+                                        source={{
+                                          uri: `http://192.168.254.139:8080/api/images/${reward.image}`,
+                                        }}
+                                      />
+                                    </View>
+                                  ) : (
+                                    <Text>Not reward</Text>
+                                  )}
+                                </View>
+                              </View>
+                            </View>
                           </View>
-                          <View className="w-full flex flex-row items-end justify-between pt-3">
-                            <Text className="text-lg text-[#00674F] font-semibold">
-                              {rewardName} {/* Display the reward name here */}
-                            </Text>
-                            <Text className="text-sm font-normal">
-                              {rewardsHistory.pointsSpent} pt.
-                            </Text>
-                          </View>
-                        </View>
-                      );
-                    })
+                        );
+                      }
+                    )
                   )}
                 </View>
               </View>
@@ -237,34 +292,81 @@ const History = () => {
                       </Text>
                     </View>
                   ) : (
-                    pointsHistory.map((pointsHistory: PointsHistory) => (
-                      <View
-                        className="w-full bg-[#E6E6E6] p-6 rounded-xl mb-4"
-                        key={pointsHistory._id}
-                      >
-                        <View className="w-full flex flex-row justify-between items-center pb-3">
-                          <Text className="text-xs font-normal text-black/50">
-                            {(() => {
-                              const date = new Date(pointsHistory.dateDisposed);
-                              return isNaN(date.getTime())
-                                ? "Invalid Date"
-                                : date.toLocaleDateString("en-US"); // Format the date as needed
-                            })()}
-                          </Text>
-                          <Text className="text-xs font-normal text-black/50 uppercase">
-                            {pointsHistory._id}
-                          </Text>
-                        </View>
-                        <View className="w-full flex flex-row items-end justify-start pt-3">
-                          <Text className="text-lg text-[#00674F] font-semibold">
-                            {pointsHistory.pointsAccumulated}{" "}
-                            {pointsHistory.pointsAccumulated > 1
-                              ? "pts."
-                              : "pt."}
-                          </Text>
-                        </View>
-                      </View>
-                    ))
+                    pointsHistory.map(
+                      (pointsHistory: PointsHistory, index: any) => {
+                        const backgroundColor =
+                          index % 2 === 0
+                            ? ["#00674F", "#06402B"]
+                            : ["#D2AF26", "#BE8400"];
+
+                        return (
+                          <View
+                            className="w-full flex-row items-center justify-center bg-[#E6E6E6] mb-4 rounded-xl overflow-hidden"
+                            key={pointsHistory._id}
+                          >
+                            <LinearGradient
+                              colors={backgroundColor}
+                              className={`h-full w-[3%]`}
+                            ></LinearGradient>
+                            <View className="w-[97%] flex items-start justify-center px-6 py-4">
+                              <View className="w-full flex flex-row justify-between items-start">
+                                <View className="w-3/4 flex items-start justify-center">
+                                  <Text
+                                    className="text-xs font-normal text-black/50 uppercase"
+                                    numberOfLines={1}
+                                  >
+                                    {pointsHistory._id}
+                                  </Text>
+
+                                  {user ? (
+                                    <Text className="text-lg font-semibold capitalize text-[#00674F]">
+                                      {`${user.personalInfo.firstName} ${user.personalInfo.lastName}`}
+                                    </Text>
+                                  ) : (
+                                    <Text className="text-lg font-semibold capitalize text-[#00674F]">
+                                      User not Found
+                                    </Text>
+                                  )}
+                                </View>
+                              </View>
+                              <View className="w-full flex flex-row justify-between items-end pt-4">
+                                <Text
+                                  className="w-1/3 text-left text-xs font-normal text-black/50"
+                                  numberOfLines={1}
+                                >
+                                  {(() => {
+                                    const date = new Date(
+                                      pointsHistory.dateDisposed
+                                    );
+                                    return isNaN(date.getTime())
+                                      ? "Invalid Date"
+                                      : date.toLocaleDateString("en-US"); // Format the date as needed
+                                  })()}
+                                </Text>
+                                <View className="w-1/2 flex flex-row items-center justify-end">
+                                  <View className="w-[40px] h-[40px] flex items-center justify-center rounded-full bg-black overflow-hidden">
+                                    <Image
+                                      className="w-full flex-1"
+                                      source={require("../../assets/images/Man.jpg")}
+                                    />
+                                  </View>
+                                  <Text
+                                    className="pl-1 text-xs font-semibold text-black"
+                                    numberOfLines={1}
+                                  >
+                                    {`${pointsHistory.pointsAccumulated}${
+                                      pointsHistory.pointsAccumulated > 1
+                                        ? "pts."
+                                        : "pt."
+                                    }`}
+                                  </Text>
+                                </View>
+                              </View>
+                            </View>
+                          </View>
+                        );
+                      }
+                    )
                   )}
                 </View>
               </View>
--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -7,9 +7,9 @@ const _layout = () => {
   return (
     <AuthProvider>
       <Stack>
-        {/* <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
-        <Stack.Screen name="(user)" options={{ headerShown: false }} /> */}
+        <Stack.Screen name="(user)" options={{ headerShown: false }} />
         <Stack.Screen name="(admin)" options={{ headerShown: false }} />
       </Stack>
     </AuthProvider>
