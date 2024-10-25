f7e54f5230f0b8f0bd52a838155d1b79f70bf97c
itzxko <134848015+itzxko@users.noreply.github.com>
2024-10-26 02:41:36 +0800

    recommit

--- Frontend/app/login.tsx
+++ Frontend/app/login.tsx
@@ -113,7 +113,6 @@ export default function Login() {
               </View>
 
               <View className="w-full flex items-start justify-center py-6">
-                <Text className="font-semibold text-sm pb-2">User Name:</Text>
                 <Text className="font-semibold text-xs pb-2">Username:</Text>
                 <View className="w-full flex flex-row items-center justify-between px-6 py-3 bg-[#E6E6E6] rounded-xl">
                   <RemixIcon name="at-line" size={16} />
